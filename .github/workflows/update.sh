697f23ecd94b9b69a67dc1647dc282ff863346ef
Harish Sheoran <harisheoran01@gmail.com>
2024-02-06 13:24:51 +0530

    update workflows

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -2,9 +2,31 @@ current_dir="."
 
 cd ./content/about || exit
 
-about_file="./../../content/about/index.md"
+file_path="./index.md"
+
+data='---
+title: "About"
+date: 2023-11-11T20:54:47+01:00
+draft: false
+description: ""
+---
+'
+
+if [ -f "$file_path" ]; then
+    # File exists, so delete it
+    rm "$file_path"
+    echo "File deleted: $file_path"
+    touch $file_path
+    echo "$data" >> "$file_path"
+else
+    touch $file_path
+    echo "$data" >> "$file_path"
+fi
+
+about_file=$file_path
 
 readme_data=$(curl -sL "https://raw.githubusercontent.com/harisheoran/harisheoran/main/README.md")
+
 
 # Set the marker after which to append the readme data
 marker="description: \"\""
@@ -12,7 +34,8 @@ marker="description: \"\""
 #Check if the marker exists in about.md
 if grep -q "$marker" "$about_file"; then
     # Append readme data after the marker in about.md using awk
-    awk -v data="$readme_data" "/$marker/{p=1;print;next} p && !NF{p=0} !p" "$about_file" > "$about_file.tmp" && mv "$about_file.tmp" "$about_file"
+    # Replace content after the marker in about.md with readme data
+    awk -v data="$readme_data" "/$marker/{print;print data;next}1" "$about_file" > "$about_file.tmp" && mv "$about_file.tmp" "$about_file"
     echo "Readme data added to $about_file after \"$marker\""
 else
     echo "Marker \"$marker\" not found in $about_file"
